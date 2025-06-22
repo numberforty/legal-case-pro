6ebf1e2a9be81c0ef484625c5e5734228647ce31
numberforty <hassanm625@gmail.com>
2025-06-22 23:49:02 +0300

    Revert "Fix runtime error in communication page"

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,4 @@
 import type { NextConfig } from 'next';
-
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const externals = Array.isArray(config.externals)
-        ? [...config.externals]
-        : config.externals
-        ? [config.externals as any]
-        : [];
-      externals.push('fluent-ffmpeg', 'whatsapp-web.js');
-      config.externals = externals;
-
 import webpack from 'webpack';
 
 const nextConfig: NextConfig = {
@@ -23,7 +7,6 @@ const nextConfig: NextConfig = {
       config.externals = Array.isArray(config.externals)
         ? [...config.externals, 'fluent-ffmpeg']
         : ['fluent-ffmpeg'];
-
     }
     return config;
   },
